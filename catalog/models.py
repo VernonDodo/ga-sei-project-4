7f21f04e0cc123b5a295d34db4d81ae40aef6c3f
VernonDodo <vcdodo@gmail.com>
2019-06-11 14:56:03 -0400

    Added an Admin model to models.py

--- catalog/models.py
+++ catalog/models.py
@@ -11,3 +11,15 @@ class User(models.Model):
     def __str__(self):
         return self.username
 
+class Admin(models.Model):
+    firstname = models.CharField(max_length=50, blank=True)
+    lastname = models.CharField(max_length=50, blank=True)
+    role = models.CharField(max_length=20, default='DBAdmin', editable=False)
+    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='admins')
+
+    def __str__(self):
+        return self.username
+
+
+
+
