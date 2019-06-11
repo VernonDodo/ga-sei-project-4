dec9ce6d5ffb825b2971fd2d4053f23a74b2d335
VernonDodo <vcdodo@gmail.com>
2019-06-11 14:16:46 -0400

    Reorganised the project to prevent build errors

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    firstname = models.CharField(max_length=50, blank=True)
+    lastname = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(max_length=50,unique=True,blank=False)
+    organisation = models.CharField(max_length=100, blank=True)
+    username = models.CharField(max_length=20, unique=True, blank=False)
+    password = models.CharField(max_length=32, default='pass4user', blank=False)
+
+    def __str__(self):
+        return self.username
+
