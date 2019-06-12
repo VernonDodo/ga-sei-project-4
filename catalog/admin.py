a6db148eaaf284218f75e0f76013860ec722d537
VernonDodo <vcdodo@gmail.com>
2019-06-12 09:38:29 -0400

    Registered the Producer model

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User, Admin
+from .models import User, Admin, Producer
 
 admin.site.register(User)
 admin.site.register(Admin)
+admin.site.register(Producer)
