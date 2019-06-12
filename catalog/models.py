6c6e9a71739dc5d9ddcb021a7f91640ec610b46b
VernonDodo <vcdodo@gmail.com>
2019-06-11 23:10:47 -0400

    Added Producer to models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,24 +1,32 @@
 from django.db import models
 
 class User(models.Model):
-    firstname = models.CharField(max_length=50, blank=True)
-    lastname = models.CharField(max_length=50, blank=True)
     email = models.EmailField(max_length=50,unique=True,blank=False)
-    organisation = models.CharField(max_length=100, blank=True)
     username = models.CharField(max_length=20, unique=True, blank=False)
     password = models.CharField(max_length=32, default='pass4user', blank=False)
 
     def __str__(self):
-        return self.username
+        return self.email
 
 class Admin(models.Model):
     firstname = models.CharField(max_length=50, blank=True)
     lastname = models.CharField(max_length=50, blank=True)
+    email = models.ForeignKey(User, on_delete=models.CASCADE, related_name='admins')
     role = models.CharField(max_length=20, default='DBAdmin', editable=False)
-    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='admins')
+    
+    def __str__(self):
+        return self.firstname
+
+class Producer(models.Model):
+    firstname = models.CharField(max_length=50, blank=True)
+    lastname = models.CharField(max_length=50, blank=True)
+    email = models.ForeignKey(User, on_delete=models.CASCADE, related_name='producers')
+    organisation = models.CharField(max_length=100, blank=False)
+    website = models.URLField(max_length=200)
 
     def __str__(self):
-        return self.username
+        return self.organisation
+
 
 
 
